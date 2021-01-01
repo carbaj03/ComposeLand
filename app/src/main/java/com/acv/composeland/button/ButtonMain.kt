b761c707db2078d91cc5e368739d5d2de9119c40
carbaj03 <carbaj03@gmail.com>
2021-01-01 16:01:02 +0100

    Button item

--- app/src/main/java/com/acv/composeland/button/ButtonMain.kt
+++ app/src/main/java/com/acv/composeland/button/ButtonMain.kt
@@ -1,18 +1,20 @@
 package com.acv.composeland.button
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.navigate
+import com.acv.composeland.R
 import com.acv.composeland.common.fakeGridItems
 import com.acv.composeland.screen.ButtonScreen
 
@@ -25,6 +27,7 @@ data class ButtonMainState(
 
 data class ButtonMainItem(
     val name: String,
+    val icon : Int,
     val goToDetail: () -> Unit = {}
 )
 
@@ -37,8 +40,14 @@ fun ButtonMain(
         goBack = { navController.popBackStack() },
         items = listOf(
             ButtonMainItem(
+                icon = R.drawable.ic_palette,
                 name = "Color",
                 goToDetail = { navController.navigate(ButtonScreen.Color.route) }
+            ),
+            ButtonMainItem(
+                icon = R.drawable.ic_click,
+                name = "Click",
+                goToDetail = { navController.navigate(ButtonScreen.Click.route) }
             )
         ),
     )
@@ -67,10 +76,39 @@ fun ButtonMain(
                             .fillMaxWidth()
                             .clickable(onClick = { screen.goToDetail() })
                     ) {
-                        Text(
-                            modifier = Modifier.padding(8.dp),
-                            text = screen.name,
-                        )
+                        ConstraintLayout(modifier = Modifier.padding(4.dp).fillMaxSize()) {
+                            val (title, description, image) = createRefs()
+
+                            Icon(
+                                modifier = Modifier
+                                    .constrainAs(image) {
+                                        start.linkTo(parent.start)
+                                        top.linkTo(parent.top)
+                                    },
+                                imageVector = vectorResource(id = screen.icon)
+                            )
+                            Text(
+                                modifier = Modifier
+                                    .constrainAs(title) {
+                                        linkTo(image.end, parent.end, bias = 0f)
+                                        width = Dimension.preferredWrapContent
+                                    },
+                                textAlign = TextAlign.Start,
+                                text = screen.name,
+                            )
+
+                            Text(
+                                modifier = Modifier
+                                    .constrainAs(description) {
+                                        linkTo(image.end, parent.end, bias = 0f)
+                                        top.linkTo(title.bottom)
+                                        width = Dimension.preferredWrapContent
+                                    },
+                                maxLines = 1,
+                                overflow = TextOverflow.Ellipsis,
+                                text = "sdfsdafdsa ssadf da f dafd",
+                            )
+                        }
                     }
                 }
             }
